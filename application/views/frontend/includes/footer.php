b6fb01965ae72c0d70de15b8f512411a1f2f4229
leandroaltuna <jhonatanaltuna@gmail.com>
2014-07-01 15:38:16 -0500

    footer keyup

--- application/views/frontend/includes/footer.php
+++ application/views/frontend/includes/footer.php
@@ -2,5 +2,41 @@
 	<script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
 	<script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
 	<script src="<?php echo base_url('bootstrap/assets/docs.min.js') ?>"></script>
+	<script type="text/javascript">
+	
+		$(function(argument) {
+
+			// $(window).keydown(function(event){
+
+			// 	if(event.keyCode == 13) {
+			// 		event.preventDefault();
+			// 		return false;
+			// 	}
+
+			// });
+
+			// $(document).on("keyup",'input,select,textarea',function(e) {    
+			// 	var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+			// 	if(key == 13)
+			// 	$(this).trigger('change');
+			// });
+
+			$(document).on("keyup",'input,select,textarea',function(e) {
+
+				var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+				var inputs = $(this).closest('form').find(":input:not(:disabled, [readonly='readonly'],:hidden)");
+				
+				if(key == 13) {
+					inputs.eq( inputs.index(this)+1).focus(); 
+				}
+				else if (key == 27) {
+					inputs.eq( inputs.index(this)-1).focus(); 
+				}
+
+			});s
+
+		});
+
+	</script>
 </body>
 </html>
